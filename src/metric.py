821cdc71841a99a88da480aa7f1bb24bca47b70f
poohoh <kjh980328@gmail.com>
2025-04-30 13:51:32 +0900

    modify f1 score

--- src/metric.py
+++ src/metric.py
@@ -2,6 +2,36 @@ from torchmetrics import Metric
 import torch
 
 # [TODO] Implement this!
+# class MyF1Score(Metric):
+#     def __init__(self, num_classes: int=200):
+#         super().__init__()
+#         self.add_state('tps', default=torch.zeros(num_classes), dist_reduce_fx='sum')
+#         self.add_state('fps', default=torch.zeros(num_classes), dist_reduce_fx='sum')
+#         self.add_state('fns', default=torch.zeros(num_classes), dist_reduce_fx='sum')
+
+#         self.eps = 1e-6
+
+#     def update(self, preds, target):
+#         # preds: B X C
+#         preds = torch.argmax(preds, dim=1)
+        
+#         for i in range(preds.shape[0]):
+#             pred_idx = preds[i]  # predicted class idx
+#             target_idx = target[i]  # target class idx
+
+#             if pred_idx == target_idx:
+#                 self.tps[pred_idx] += 1
+#             else:
+#                 self.fps[pred_idx] += 1
+#                 self.fns[target_idx] += 1
+    
+#     def compute(self):
+#         precision = self.tps / (self.tps + self.fps + self.eps)  # TP / (TP + FP)
+#         recall = self.tps / (self.tps + self.fns + self.eps)  # TP / (TP + FN)
+#         f1 = 2 * (precision * recall) / (precision + recall + self.eps)  # B x 1
+
+#         return f1.mean().item()  # average f1 score
+
 class MyF1Score(Metric):
     def __init__(self, num_classes: int=200):
         super().__init__()
@@ -10,27 +40,40 @@ class MyF1Score(Metric):
         self.add_state('fns', default=torch.zeros(num_classes), dist_reduce_fx='sum')
 
         self.eps = 1e-6
+        self.num_classes = num_classes
 
     def update(self, preds, target):
         # preds: B X C
         preds = torch.argmax(preds, dim=1)
-        
-        for i in range(preds.shape[0]):
-            pred_idx = preds[i]  # predicted class idx
-            target_idx = target[i]  # target class idx
 
-            if pred_idx == target_idx:
-                self.tps[pred_idx] += 1
-            else:
-                self.fps[pred_idx] += 1
-                self.fns[target_idx] += 1
+        preds = preds.flatten()
+        target = target.flatten()
+
+        if preds.shape != target.shape:
+            raise ValueError(f"shape mismatch")
+
+        tp_mask = (preds == target)
+        fp_mask = (preds != target)
+
+        tp_indices = target[tp_mask]
+        tps_batch = torch.bincount(tp_indices, minlength=self.num_classes)
+
+        fp_indices = preds[fp_mask]
+        fps_batch = torch.bincount(fp_indices, minlength=self.num_classes)
+
+        fn_indices = target[fp_mask]
+        fns_batch = torch.bincount(fn_indices, minlength=self.num_classes)
+
+        self.tps += tps_batch.to(self.tps.device)
+        self.fps += fps_batch.to(self.fps.device)
+        self.fns += fns_batch.to(self.fns.device)
     
     def compute(self):
-        precision = self.tps / (self.tps + self.fps + self.eps)  # TP / (TP + FP)
-        recall = self.tps / (self.tps + self.fns + self.eps)  # TP / (TP + FN)
-        f1 = 2 * (precision * recall) / (precision + recall + self.eps)  # B x 1
+        precision = self.tps / (self.tps + self.fps + self.eps)
+        recall = self.tps / (self.tps + self.fns + self.eps)
+        f1_per_class = 2 * (precision * recall) / (precision + recall + self.eps)
 
-        return f1.mean().item()  # average f1 score
+        return f1_per_class.mean().item()  # average f1 score
 
 class MyAccuracy(Metric):
     def __init__(self):
